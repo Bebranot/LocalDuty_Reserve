80b9e2338ab25b63bea5d70951753581a973e9b3
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-11-23 18:38:44 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Orion/ServerProtection/ServerProtectionPunishmentSystem.cs
+++ Content.Server/_Orion/ServerProtection/ServerProtectionPunishmentSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Net;
 using System.Net.Sockets;
 using Content.Server.Administration;
