76ec95d2be6f6563e5a22ea9231f08a27bc9dd9f
echotry <48294642+echotry-ss14@users.noreply.github.com>
2026-01-25 21:25:34 +0300

    Revert reverta + reset to Jan 5, 2026 (kinda cringe) (#206)
    * revert awarecringe
    
    * Revert "Revert upstream 21 01 2026 (#205)"
    
    This reverts commit 36afd69fc4e446c9f610791bc3acc5ddcae7a81a.
    
    * revert revert + reset to 0780702 (Jan 5, 2026)
    
    ---------
    
    Co-authored-by: echotry-ss14 <poshol.naxyi@obosrail.com>

--- Content.Shared/_Shitcode/Heretic/Systems/SharedFireBlastSystem.cs
+++ Content.Shared/_Shitcode/Heretic/Systems/SharedFireBlastSystem.cs
@@ -7,6 +7,7 @@ using Content.Goobstation.Common.Physics;
 using Content.Shared._Shitcode.Heretic.Components;
 using Content.Shared._Shitmed.Damage;
 using Content.Shared._Shitmed.Targeting;
+using Content.Shared.Body.Systems;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Systems;
 using Content.Shared.StatusEffectNew;
@@ -20,6 +21,7 @@ public abstract class SharedFireBlastSystem : EntitySystem
     [Dependency] protected readonly SharedTransformSystem Xform = default!;
     [Dependency] protected readonly StatusEffectsSystem Status = default!;
     [Dependency] protected readonly DamageableSystem Dmg = default!;
+    [Dependency] protected readonly SharedBodySystem Body = default!;
 
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedStaminaSystem _stam = default!;
@@ -84,7 +86,7 @@ public abstract class SharedFireBlastSystem : EntitySystem
             };
 
             Dmg.TryChangeDamage(uid,
-                damage,
+                damage * Body.GetVitalBodyPartRatio(uid),
                 true,
                 false,
                 dmg,
