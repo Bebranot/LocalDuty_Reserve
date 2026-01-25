d5e8082b7196d0ce9c5dcc1479284d846d25c60b
echotry-ss14 <poshol.naxyi@obosrail.com>
2026-01-25 21:19:34 +0300

    Revert "Revert upstream 21 01 2026 (#205)"
    This reverts commit 36afd69fc4e446c9f610791bc3acc5ddcae7a81a.

--- Content.Shared/_Lavaland/Megafauna/Selectors/ProtoIdMegafaunaSelector.cs
+++ Content.Shared/_Lavaland/Megafauna/Selectors/ProtoIdMegafaunaSelector.cs
@@ -10,6 +10,7 @@ namespace Content.Shared._Lavaland.Megafauna.Selectors;
 /// Works like NestedMegafaunaSelector, but also has serialization,
 /// so you can just type id: in prototypes and it will work.
 /// </summary>
+[DataDefinition]
 public sealed partial class ProtoIdMegafaunaSelector : MegafaunaSelector
 {
     public const string IdDataFieldTag = "id";
--- Content.Shared/_Shitcode/Heretic/Systems/Abilities/SharedHereticAbilitySystem.Void.cs
+++ Content.Shared/_Shitcode/Heretic/Systems/Abilities/SharedHereticAbilitySystem.Void.cs
@@ -73,7 +73,7 @@ public abstract partial class SharedHereticAbilitySystem
             if (condition)
                 Voidcurse.DoCurse(pookie, 2);
             _dmg.TryChangeDamage(pookie,
-                args.Damage,
+                args.Damage * _body.GetVitalBodyPartRatio(pookie),
                 true,
                 origin: ent,
                 targetPart: TargetBodyPart.All,
@@ -96,7 +96,7 @@ public abstract partial class SharedHereticAbilitySystem
         foreach (var pookie in pookies)
         {
             _dmg.TryChangeDamage(pookie,
-                args.Damage,
+                args.Damage * _body.GetVitalBodyPartRatio(pookie),
                 true,
                 origin: ent,
                 targetPart: TargetBodyPart.All,
