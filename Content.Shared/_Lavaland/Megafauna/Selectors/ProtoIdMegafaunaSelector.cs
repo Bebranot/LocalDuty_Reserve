36afd69fc4e446c9f610791bc3acc5ddcae7a81a
Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
2026-01-25 22:20:05 +0500

    Revert upstream 21 01 2026 (#205)
    * Revert "Merge pull request #193 from Kutosss/upstream-goob-21-01-2026"
    
    This reverts commit 84ee9a963205bf73fe4527f6ceafdf68965c690c, reversing
    changes made to a85a6ffe000957780b17e8de530a627f344dc0c1.
    
    * chore: Automatically update REUSE headers
    
    ---------
    
    Co-authored-by: ReserveBot <211949879+ReserveBot@users.noreply.github.com>

--- Content.Shared/_Lavaland/Megafauna/Selectors/ProtoIdMegafaunaSelector.cs
+++ Content.Shared/_Lavaland/Megafauna/Selectors/ProtoIdMegafaunaSelector.cs
@@ -10,7 +10,6 @@ namespace Content.Shared._Lavaland.Megafauna.Selectors;
 /// Works like NestedMegafaunaSelector, but also has serialization,
 /// so you can just type id: in prototypes and it will work.
 /// </summary>
-[DataDefinition]
 public sealed partial class ProtoIdMegafaunaSelector : MegafaunaSelector
 {
     public const string IdDataFieldTag = "id";
