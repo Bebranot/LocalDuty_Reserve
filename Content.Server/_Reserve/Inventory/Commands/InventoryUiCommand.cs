2104c5c1202e8ab14815a58bc4ec438f7208e462
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2026-02-22 10:42:55 +0000

    chore: Automatically update REUSE headers

--- Content.Server/_Reserve/Inventory/Commands/InventoryUiCommand.cs
+++ Content.Server/_Reserve/Inventory/Commands/InventoryUiCommand.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server._Reserve.Inventory.UI;
 using Content.Server.EUI;
 using Content.Shared._Reserve.LenaApi;
