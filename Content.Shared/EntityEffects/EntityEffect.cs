a351b6cbce62ce1d01c5bf5cc21314ec42051541
Svarshik <lexasvarshik666@gmail.com>
2026-02-25 18:09:00 +0500

    EntityEffect upstream fix

--- Content.Shared/EntityEffects/EntityEffect.cs
+++ Content.Shared/EntityEffects/EntityEffect.cs
@@ -6,6 +6,7 @@
 
 using System.Linq;
 using Content.Shared.Chemistry;
+using Robust.Shared.Serialization;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Database;
 using Content.Goobstation.Maths.FixedPoint;
@@ -21,6 +22,7 @@ namespace Content.Shared.EntityEffects;
 ///     Entity effects describe behavior that occurs on different kinds of triggers, e.g. when a reagent is ingested and metabolized by some
 ///     organ. They only trigger when all of <see cref="Conditions"/> are satisfied.
 /// </summary>
+[Serializable, NetSerializable]
 [ImplicitDataDefinitionForInheritors]
 [MeansImplicitUse]
 public abstract partial class EntityEffect
